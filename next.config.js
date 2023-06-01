9c48b0e18f6e550bebcac81c30a4eb00312919d9
Julio Bazalar <jbzuloaga@gmail.com>
2023-06-01 10:49:08 -0500

    Add basic file ordering for i18n

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ["en-US", "es-ES"],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path.
+     */
+    defaultLocale: "en-US",
+  },
 }
 
 module.exports = nextConfig
